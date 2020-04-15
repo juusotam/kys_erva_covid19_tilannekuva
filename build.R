3be44f5ef8200ba2219f7b669086673c1c39dcc7
Juuso Tamminen <juuso.tamminen@juksu.net>
2020-04-15 11:19:28 +0300

    Debugged

--- build.R
+++ build.R
@@ -2,5 +2,8 @@
 
 #rmarkdown::render("raportti.Rmd", output_file = paste0("KYS ERVA COVID-19 raportti - ", Sys.Date(),".docx"), output_dir = "output")
 
-rmarkdown::render("aikasarja.Rmd", output_file = paste0("KYS ERVA COVID-19 raportti - ", Sys.Date(),".pdf"), output_dir = "output", knit_root_dir = getwd(), intermediates_dir = getwd())
-
+rmarkdown::render("aikasarja.Rmd", output_file = paste0("KYS ERVA COVID-19 raportti - ", Sys.Date(),".pdf"), 
+                  output_dir = "output", 
+                  knit_root_dir = getwd(), 
+                  intermediates_dir = getwd(),
+                  clean = FALSE)
